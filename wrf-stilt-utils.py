4573da2f57afc39d6f5cb38b54b88f953496cebc
Sean Crowell <seanmrcrowell@gmail.com>
2024-10-14 15:49:26 -0500

    added return variables to sample_wrfout_ghg_point to build a pipeline

--- wrf-stilt-utils.py
+++ wrf-stilt-utils.py
@@ -1,38 +1,8 @@
+"""
 #========================
 # wrf-stilt-utils.py 
-#
-# Changelog:
-# 10/03/2024 V0: Initial Commit - Sean Crowell
-#
-# Contains:
-# 
-# sample_wrfout_single_receptor(wrf_domain='',boundary_filename='')
-#    Description:       Finds the wrfout file with the location/time of a particle and 
-#                       extracts its CO2 and CH4 at the nearest model level with KDTree
-#    Inputs:
-#    wrf_domain:        wrfout domain - "d01"
-#    boundary_filename: the boundary point file name - contains the location and time
-#                       where each particle hit the boundary
-#    
-# locate_trajectory_boundary_points(traj_fname='',save_dir='',bbox=[],write_files=False)
-#    Description:       Locates the location/time where the particle trajectory first
-#                       leaves a predefined lat/lon rectangular box and writes it to a 
-#                       file.
-#    Inputs:
-#    traj_fname:        Particle trajectory .rds filename
-#    save_dir:          Where to save the boundary point file
-#    bbox:              list of four values: [lon_lb,lon_ub,lat_lb,lat_ub]
-#
-# run_function_in_parallel(fun,args_list)
-#    Description:       Generic function that runs any function over a set of CPUs -
-#                       note that this uses concurrent.futures.PoolProcessExecutor due
-#                       to multithreading issues with rpy2
-#    Inputs:
-#    fun:               Python function
-#    args_list:         List of dictionaries with keys set to match the input variable
-#                       argument names for the function fun
-#
 #========================
+"""
 
 from h5py import File
 import netCDF4 as nc
@@ -45,11 +15,21 @@ import rpy2.robjects as ro
 from rpy2.robjects import conversion,default_converter
 
 def sample_wrfout_met_profile(wrf_domain='d01',wrf_path='',var_dict={}):
-    ''' 
-    This function finds the wrfout files closest in time to a sample and
-    then locates the gridbox closest to the sample location from which it 
-    pulls the variables of interest. 
-    '''
+    """
+    Description:        This function finds the wrfout files closest in time to a sample and
+                        then locates the gridbox closest to the sample location from which it 
+                        pulls the variables of interest.
+
+    Inputs:
+        wrf_domain      A string like d01 or d02 - controls which domain you sample from
+        wrf_path        Where the wrfout files are stored
+        var_dict        Contains the physical locations and times to sample, can also contain 
+                        vertical levels if vertical interpolation to preset levels is implied
+                        Example: {'lat':[30],'lon':[-75],'datetime':datetime(2023,7,1,18,30),'levs':[10,100]}
+
+    Returns:
+        var_dict        Augmented dictionary with variables from the wrfout file of interest on levels
+    """
     files = sorted(glob.glob(f'{wrf_path}/*{wrf_domain}*'))
     wrfdatetime = [dt.datetime.strptime(fi.split(f'{wrf_domain}_')[-1],'%Y-%m-%d_%H:%M:%S') for fi in files]
     try:
@@ -125,11 +105,24 @@ def sample_wrfout_met_profile(wrf_domain='d01',wrf_path='',var_dict={}):
 
     return var_dict
 
-def sample_wrfout_single_receptor(wrf_domain='d01',boundary_filename='',overwrite=False):
-
+def sample_wrfout_point_ghg(wrf_domain='d01',wrf_path='',boundary_filename='',overwrite=False):
+    """
+    Description:            Finds the wrfout file with the location/time of a particle and 
+                            extracts its CO2 and CH4 at the nearest model level with KDTree
+                            and writes the information to a file. Assumes hourly wrf output.
+    Inputs:
+        wrf_domain:         wrfout domain - "d01"
+        wrf_path:           directory where wrfout files are stored
+        boundary_filename:  the boundary point file name - contains the location and time
+                            where each particle hit the boundary
+        overwrite           True means that any existing files are overwitten
+    Returns:
+        bc                  dictionary containing the boundary co2 and ch4 from wrf as well
+                            as the wrf lat,lon,z closest to the sample
+    """
     wrf_bnd_fname = boundary_filename.split('.h5')[0]+'_wrf'+wrf_domain+'.h5'
     if os.path.exists(wrf_bnd_fname):
-        if overwrite==True: 
+        if overwrite==True:
             os.remove(wrf_bnd_fname)
         else:
             print('Boundary GHG file already exists, skipping.')
@@ -140,23 +133,22 @@ def sample_wrfout_single_receptor(wrf_domain='d01',boundary_filename='',overwrit
         lon = loc_f['particle_longitude'][:]
         lat = loc_f['particle_latitude'][:]
         t = loc_f['particle_time'][:]
-        obs_t = (dt.datetime.strptime(loc_f.attrs['obs_time'][:],'%Y-%m-%d %H:%M:%S UTC')-dt.datetime(1970,1,1)).total_seconds()
+        obs_t = (dt.datetime.strptime(loc_f.attrs['obs_time'][:], \
+                '%Y-%m-%d %H:%M:%S UTC')-dt.datetime(1970,1,1)).total_seconds()
         loc_f.close()
 
     #==================================================
-    wrfout_prefix = '/work2/07655/tg869546/stampede3/nyc-chem/2023/wrfout/'
+    #wrfout_prefix = '/work2/07655/tg869546/stampede3/nyc-chem/2023/wrfout/'
     part_t = np.array([dt.datetime(1970,1,1) + dt.timedelta(seconds=ti) for ti in t])
-    wrf_files = np.array([f"{wrfout_prefix}/wrfout_{wrf_domain}_{part_t[ip].strftime('%Y-%m-%d_%H')}:00:00" for ip in range(len(part_t))])
+    wrf_files = np.array([f"{wrf_path}/wrfout_{wrf_domain}_{part_t[ip].strftime('%Y-%m-%d_%H')}:00:00" \
+        for ip in range(len(part_t))])
     # Only loop over the unique filenames to save I/O
     unique_wrf_files = sorted(np.array(list(set(wrf_files))))
     #===================================================
-
-    bc_lat = np.zeros(lat.shape)# NN lat of WRF model
-    bc_lon = np.zeros(lat.shape)  # NN lon of WRF model
-    bc_z = np.zeros(lat.shape)    # NN altitude of WRF model
-    bc_co2 = np.zeros(lat.shape)  # CO2 at WRF 3D NN gridbox
-    bc_ch4 = np.zeros(lat.shape)  # CH4 at WRF 3D NN gridbox
-    bc_t = ['' for i in range(len(lat))] # WRFOUT file time
+    bc = {}
+    for v in ['lat','lon','z','co2','ch4']:
+        bc[v] = np.zeros(lat.shape)
+    bc['t'] = ['' for i in range(len(lat))] # WRFOUT file time
     for fi in unique_wrf_files:
         f = nc.Dataset(fi,'r')
         wrf_lat = f['XLAT'][:][0]
@@ -181,43 +173,59 @@ def sample_wrfout_single_receptor(wrf_domain='d01',boundary_filename='',overwrit
 
         dd,ii = tree.query(part_points,k=1,eps=0.0)
         lat_inds,lon_inds = np.unravel_index(ii,wrf_lat.shape,order='C')
-        bc_lat[part_inds] = wrf_lat[lat_inds,lon_inds]
-        bc_lon[part_inds] = wrf_lon[lat_inds,lon_inds]
+        bc['lat'][part_inds] = wrf_lat[lat_inds,lon_inds]
+        bc['lon'][part_inds] = wrf_lon[lat_inds,lon_inds]
         for i in part_inds:
-            bc_t[i] = fi.split('/')[-1]
+            bc['t'][i] = fi.split('/')[-1]
 
         sub_z = wrf_z[:,lat_inds,lon_inds] - wrf_zsurf[lat_inds,lon_inds]
         z_inds = np.array([np.argmin((part_z[ip]-sub_z[:,ip])**2) for ip in range(len(part_inds))])
-        bc_z[part_inds] = wrf_z[z_inds,lat_inds,lon_inds] - wrf_zsurf[lat_inds,lon_inds]
+        bc['z'][part_inds] = wrf_z[z_inds,lat_inds,lon_inds] - wrf_zsurf[lat_inds,lon_inds]
 
         co2 = np.zeros(len(part_inds))
         for co2_v in ['ANT','BIO','OCE']:
             co2 += f['CO2_'+co2_v][0][z_inds,lat_inds,lon_inds]-f['CO2_BCK'][0][z_inds,lat_inds,lon_inds]
         co2 += f['CO2_BCK'][0][z_inds,lat_inds,lon_inds]
-        bc_co2[part_inds] = co2[:]
+        bc['co2'][part_inds] = co2[:]
 
         ch4 = np.zeros(len(part_inds))
         for ch4_v in ['ANT','BIO']:
             ch4 += f['CH4_'+ch4_v][0][z_inds,lat_inds,lon_inds]-f['CH4_BCK'][0][z_inds,lat_inds,lon_inds]
         ch4 += f['CH4_BCK'][0][z_inds,lat_inds,lon_inds]
-        bc_ch4[part_inds] = ch4[:]
+        bc['ch4'][part_inds] = ch4[:]
+    bc['part_lat'] = lat[:]
+    bc['part_lon'] = lon[:]
+    bc['part_z'] = alt[:]
+    bc['part_t'] = t[:]
     with File(wrf_bnd_fname,'w') as loc_f:
         loc_f.create_dataset('part_lat',data=lat[:])
         loc_f.create_dataset('part_lon',data=lon[:])
         loc_f.create_dataset('part_z',data=alt[:])
         loc_f.create_dataset('part_t',data=t[:])
-        loc_f.create_dataset('wrf_lat',data=bc_lat[:])
-        loc_f.create_dataset('wrf_lon',data=bc_lon[:])
-        loc_f.create_dataset('wrf_z',data=bc_z[:])
-        loc_f['wrf_time'] = bc_t[:]
-        loc_f.create_dataset('wrf_co2',data=bc_co2[:])
-        loc_f.create_dataset('wrf_ch4',data=bc_ch4[:])
+        loc_f.create_dataset('wrf_lat',data=bc['lat'][:])
+        loc_f.create_dataset('wrf_lon',data=bc['lon'][:])
+        loc_f.create_dataset('wrf_z',data=bc['z'][:])
+        loc_f['wrf_time'] = bc['t'][:]
+        loc_f.create_dataset('wrf_co2',data=bc['co2'][:])
+        loc_f.create_dataset('wrf_ch4',data=bc['ch4'][:])
         loc_f.close()
+    return bc
 
 def locate_trajectory_boundary_points(traj_fname='',save_dir='',bbox=[],write_files=False):
-
-    save_fname = save_dir+traj_fname.split('/')[-1].split('.rds')[0]+'_bnd.h5'
-    lon_lb,lon_ub,lat_lb,lat_ub = bbox[:]#args_dict['bounding_box']
+    """
+    Description:        Finds the place in an RDS file called traj_fname from STILT that a particle leaves a 
+                        bounding box `bbox`
+    Inputs:
+        traj_fname      Filename of RDS file from STILT that gives the particle information
+        save_dir        Directory where boundary H5 files are saved
+        bbox            List [Longitude Lower Bound, Longitude Upper Bound, Latitude Lower Bound, Latitude Upper Bound]
+        write_files     Boolean that controls whether files get written out as part of the routine
+    
+    Returns:
+        part            Dict (keys): time (t), particle ID (ind), latitude (lat), longitude (lon), and altitude (zagl)
+        bnd             Dict (keys): x, y, z, t of intersection, observation time (obs_t)
+    """
+    lon_lb,lon_ub,lat_lb,lat_ub = bbox[:]
 
     with conversion.localconverter(default_converter):
 #Trajectories are saved as RDS File
@@ -226,20 +234,17 @@ def locate_trajectory_boundary_points(traj_fname='',save_dir='',bbox=[],write_fi
 #----
 # Particle information
         part = {}
-# t = time in seconds since release (from minutes in RDF file)
-        part['t'] = np.array(rdf[2][0])*60
-# ind = particle ID
-        part['ind'] = np.array(rdf[2][1])
-# lat/lon/zagl = physical location of particle at a given time step
-        part['lat'] = np.array(rdf[2][3])
-        part['lon'] = np.array(rdf[2][2])
-        part['zagl'] = np.array(rdf[2][4])
-        n_traj = int(part['ind'].max())
+        part['t'] = np.array(rdf[2][0])*60  # t = time in seconds since release (from minutes in RDF file)
+        part['ind'] = np.array(rdf[2][1])   # ind = particle ID
+        part['lat'] = np.array(rdf[2][3])   # lat/lon/zagl = physical location of particle at a given time step
+        part['lon'] = np.array(rdf[2][2])   # lat/lon/zagl = physical location of particle at a given time step
+        part['zagl'] = np.array(rdf[2][4])  # lat/lon/zagl = physical location of particle at a given time step
+        n_traj = int(part['ind'].max())     # number of trajectories
 #----
 
 #----
 # Find boundary intersection
-    bnd_inds = []
+    bnd_inds = []                           # index where particles first hit the boundary of the bbox
     for i in range(1,n_traj+1):
         pt_inds = np.where(part['ind'] == i)[0]
         bind = np.where((part['lon'][pt_inds] >= lon_lb)*(part['lon'][pt_inds] <= lon_ub)*(part['lat'][pt_inds] <= lat_ub)*(part['lat'][pt_inds] >= lat_lb))[0]
@@ -273,8 +278,9 @@ def locate_trajectory_boundary_points(traj_fname='',save_dir='',bbox=[],write_fi
     bnd['obs_t'] = rdf[1][0][0] #(dt.datetime(1970,1,1) + dt.timedelta(seconds=rdf[1][0][0])).strftime('%Y-%    m-%d %H:%M:%S UTC')
     bnd['part_t'] = bnd['obs_t'] + bnd['t'] # Seconds since 1970,1,1
 
-    if write_files: 
-        f_out = File('bnd/'+bnd_fname,'w')
+    if write_files:
+        save_fname = save_dir+traj_fname.split('/')[-1].split('.rds')[0]+'_bnd.h5'
+        f_out = File(save_fname,'w')
         f_out.attrs['obs_time'] = (dt.datetime(1970,1,1) + dt.timedelta(seconds=rdf[1][0][0])).strftime('%Y-%m-%d %H:%M:%S UTC')
         f_out.create_dataset('particle_time',data=bnd['part_t'][:])
         f_out['particle_time'].attrs['units'] = 'Seconds since 1/1/1970'
@@ -286,6 +292,15 @@ def locate_trajectory_boundary_points(traj_fname='',save_dir='',bbox=[],write_fi
     return part,bnd
 
 def run_function_in_parallel(fun,args_list):
+    """
+    Description:       Generic function that runs any function over a set of CPUs -
+                       note that this uses concurrent.futures.PoolProcessExecutor due
+                       to multithreading issues with rpy2
+    Inputs:
+        fun            Python function
+        args_list      List of dictionaries with keys set to match the input variable
+                       argument names for the function fun
+    """
     with concurrent.futures.ProcessPoolExecutor() as executor:
         # Submit tasks to be executed in parallel with named arguments
         future_to_args = {executor.submit(fun, **args): args for args in args_list}
